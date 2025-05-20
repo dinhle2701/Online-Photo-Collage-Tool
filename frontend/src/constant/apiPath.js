79564ce2c8e20a66efd850982e2f9897b1e684b7
dinh-letat <dinhprococoa@gmail.com>
2025-05-20 18:19:13 +0700

    refactors: change apiPath

--- frontend/src/constant/apiPath.js
+++ frontend/src/constant/apiPath.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://98.81.206.22:5000"
+// const API_BASE_URL = "http://98.81.206.22:5000"
+const API_BASE_URL = "http://localhost:5000"
 
 export const API_PATHS = {
     CREATE_COLLAGE: `${API_BASE_URL}/create-task`,  // Thêm đường dẫn API tạo ảnh ghép
