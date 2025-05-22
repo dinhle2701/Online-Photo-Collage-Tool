93fb796a5e8f4e20734a81a29fbf6bcd2964d923
dinh-letat <dinhprococoa@gmail.com>
2025-05-22 10:44:53 +0700

    fix: update responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App bg-slate-200 px-4 sm:px-12 leading-relaxed min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-[30%_1fr] gap-y-4 md:gap-y-0 gap-x-6 md:gap-x-10 min-h-[80vh]">
+      <div className="grid grid-cols-1 md:grid-cols-[30%_1fr] gap-y-4 md:gap-y-0 gap-x-6 md:gap-x-10 min-h-[80vh] pt-10">
         <Sidebar setCollageId={setTaskId} />
         <Content collageId={taskId} />
       </div>
